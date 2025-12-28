2f0daf60a1cd884914b445bfd5a979fb3c074a7c
Rafael Italiano <53285653+rafael-italiano@users.noreply.github.com>
2025-12-28 05:50:10 +0000

    fix: vercel deploy
    * fix: vercel deploy
    
    * fix: vercel deploy
    
    * fix: vercel deploy
    
    * fix: vercel deploy
    
    * fix: vercel deploy
    
    * adjust dev to match prod runtime

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,7 @@ podman run \
     -p 8080:8000 \
     -v ./src/skogul/:/app/skogul/ \
     -v ./src/manage.py:/app/manage.py \
+    --env-file .env \
     skogul-django \
     python -m gunicorn skogul.wsgi:application \
         --bind 0.0.0.0:8000 \
