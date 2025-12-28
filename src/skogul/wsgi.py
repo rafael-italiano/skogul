82a7c0975f46e2a4fdb69f287f10cbdc5777ea17
Rafael Italiano <rafaeljitaliano@gmail.com>
2025-12-28 02:30:42 -0300

    fix: vercel deploy

--- src/skogul/wsgi.py
+++ src/skogul/wsgi.py
@@ -8,10 +8,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(BASE_DIR))
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "skogul.settings")
 
 application = get_wsgi_application()
-app = application
